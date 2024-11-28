3d232fa6a60dad3b3c09063e6f772e20be8e6cbc
DoThanhNhan0701 <tnhan.ittechnology@gmail.com>
2024-11-28 22:37:04 +0700

    feat: update-image

--- src/components/pages/Client/Setting/index.tsx
+++ src/components/pages/Client/Setting/index.tsx
@@ -9,6 +9,47 @@ import CreateTopic from './_component/CreateTopic'
 import React from 'react'
 import { useRouter } from 'next/navigation'
 
+const dataTopic = [
+  {
+    name: 'Animal',
+    image:
+      'https://motherspet.com/blogs/wp-content/webp-express/webp-images/uploads/2024/07/100-wild-animals-870x490.jpg.webp',
+  },
+  {
+    name: 'Fruit',
+    image:
+      'https://www.ninosalvaggio.com/wp-content/uploads/2023/07/Ninos_What-Makes-A-Fruit-A-Fruit-1024x683.jpg',
+  },
+  {
+    name: 'Bird',
+    image:
+      'https://i0.wp.com/echoofwings.com/wp-content/uploads/2024/06/most-colorful-birds.jpg?w=615&ssl=1',
+  },
+  {
+    name: 'Jobs',
+    image:
+      'https://tailieutienganh.edu.vn/public/files/upload/default/images/phu-am-danh-tu-dem-duoc-so-it-so-nhieu-khong-dem-duoc-tu-vung-nghe-nghiep-jobs-3.jpg',
+  },
+  {
+    name: 'Club Football',
+    image: 'https://img.jagranjosh.com/images/2024/February/622024/World-Most-Valuable-Club.webp',
+  },
+  {
+    name: 'Sports',
+    image:
+      'https://cdn.thuvienphapluat.vn//uploads/Hoidapphapluat/2023/PHCM/Thang01/17-1/the-duc-the-thao.jpg',
+  },
+  {
+    name: 'Influencer',
+    image: 'https://thegioimarketing.vn/wp-content/uploads/2023/01/influencers-la-gi.png',
+  },
+  {
+    name: 'Country',
+    image:
+      'https://img.freepik.com/free-vector/flags-different-countries-speech-bubble-shape_23-2147862139.jpg?semt=ais_hybrid',
+  },
+]
+
 export default function SettingPage() {
   const router = useRouter()
   const { isOpen, closeModal, openModal } = useModal()
@@ -36,22 +77,21 @@ export default function SettingPage() {
           <h4 className="py-4 font-extrabold text-2xl text-center uppercase">Setting User</h4>
           <div className="flex justify-center w-full">
             <Image
-              className="rounded-full "
+              className="rounded-full border"
               preview={false}
               width={100}
-              src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+              src="https://cdn.vectorstock.com/i/1000x1000/44/01/default-avatar-photo-placeholder-icon-grey-vector-38594401.webp"
             />
           </div>
           <div>
-            <p className="font-bold text-base">Name: ---</p>
-            <p className="font-bold text-base">Sound: ---</p>
-            <p className="font-bold text-base">Language: ---</p>
-            <p className="font-bold text-base">Voice: ---</p>
+            <p className="font-bold text-base">Username: NguyenHoangLinh</p>
+            <p className="font-bold text-base">Email: linh_2051220165@dau.edu.vn</p>
+            <p className="font-bold text-base">Language: English</p>
           </div>
         </div>
         <h1 className="text-center py-6 text-2xl font-bold">Personal Topic</h1>
         <div className="flex gap-4 w-full overflow-x-auto">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, inx) => (
+          {dataTopic.map((item, inx) => (
             <React.Fragment key={inx}>
               <div
                 onClick={() => router.push(`/settings/${inx}`)}
@@ -63,19 +103,20 @@ export default function SettingPage() {
                 />
                 <div className="flex justify-center">
                   <Image
-                    className="rounded-full"
+                    className="rounded-full object-cover border"
                     preview={false}
                     width={100}
-                    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                    height={100}
+                    src={item.image}
                   />
                 </div>
                 <div className="flex justify-center mt-4 flex-col items-center">
-                  <p className="font-bold text-base">{`Fruit: ${item}`}</p>
+                  <p className="font-bold text-base">{`${item.name}`}</p>
                   <p className="font-bold text-base">300 Image</p>
                 </div>
               </div>
 
-              {inx === 9 && (
+              {inx === 7 && (
                 <div
                   key="create-topic"
                   onClick={openModal}
